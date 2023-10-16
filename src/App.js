6dde54cffb1ecac6f4d610e8f3b3752858a6995b
David Tang <687449+iamdtang@users.noreply.github.com>
2023-10-15 17:47:34 -0700

    Debouncing v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+
+function fetchRepos(q) {
+  return fetch(`https://api.github.com/search/repositories?q=${q}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      return json.items.map((item) => {
+        return item.full_name;
+      });
+    });
+}
 
 function App() {
+  const [q, setQ] = useState("");
+  const [options, setOptions] = useState([]);
+
+  const timeoutRef = useRef(undefined);
+  // let timeout = undefined;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        list="fruits"
+        value={q}
+        onChange={(event) => {
+          const value = event.target.value;
+
+          setQ(value);
+
+          // if a timeout exists
+          if (timeoutRef.current !== undefined) {
+            clearTimeout(timeoutRef.current);
+          }
+
+          if (value) {
+            timeoutRef.current = setTimeout(() => {
+              fetchRepos(value).then((repoNames) => {
+                setOptions(repoNames);
+              });
+            }, 1000);
+          } else {
+            setOptions([]);
+          }
+        }}
+      />
+      <datalist id="fruits">
+        {options.map((option) => {
+          return <option value={option} key={option} />;
+        })}
+      </datalist>
     </div>
   );
 }
